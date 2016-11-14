75ad3aff0754a44b9967e1f7464aa1ae8524bab4
Balazs Nemeth <nemethbalazs2000@gmail.com>
2016-11-14 12:03:57 +0100

    Added fizz.cpp, which prints the positive integers from 1 to 100

---
+++ fizz.cpp
@@ -0,0 +1,13 @@
+// fizz.cpp
+// Prints positive integers from 1 to 100
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 100; ++i)
+    {
+        cout << i << endl;
+    }
+}
